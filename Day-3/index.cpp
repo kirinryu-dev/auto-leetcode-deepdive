d90d1261faa9436edf9dc0a31da55e0bd1e89870
kirinryu-dev <kirinryu.dev@gmail.com>
2026-03-10 22:47:51 +0000

    ...

--- Day-3/index.cpp
+++ Day-3/index.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 
+
+// class
 class Secret {
 public:
    std::string name ;
@@ -12,6 +14,26 @@ public:
    void sayHi(){
     std::cout << "Hiii!!!" << std::endl ;
 
-   };
-   
+   }
+
 };
+// struct 
+struct Person {
+    std::string name ;
+    int age ;
+
+};
+
+int main () {
+    // struct 
+    Person P1{"Alice", 25} ;
+    std::cout << P1.name << std::endl ;
+    std::cout << P1.age << std::endl ;
+    
+    // class
+    Secret mySecret ;
+    mySecret.name = "secret agent 007" ;
+    std::cout << "Im secret agent : " << mySecret.name << std::endl ;
+    
+
+}
